c69ddcf3b02330983aaa6595954ad0d038630853
Romain Benmansour <romain.benmansour@gmail.com>
2023-06-10 12:25:25 +0200

    :lipstick: Add Sample list for Homepage

--- app/src/main/java/com/icarie/base/navigation/MainGraph.kt
+++ app/src/main/java/com/icarie/base/navigation/MainGraph.kt
@@ -1,24 +1,27 @@
 package com.icarie.base.navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import com.icarie.base.app.MainViewModel
 import com.icarie.base.navigation.common.composable
 import com.icarie.base.navigation.common.navigation
 import com.icarie.base.ui.compose.UIScreen
-import com.icarie.base.ui.compose.composables.NetworkStateModal
 import com.icarie.base.ui.compose.composables.Title
 import com.icarie.base.ui.compose.composables.UIStateView
+import com.icarie.base.ui.compose.config.AppSpacings
 import com.icarie.base.ui.compose.states.UIState
 import com.icarie.base.ui.home.MainScreenUIData
 
@@ -40,17 +43,33 @@ private fun NavGraphBuilder.homeScreen() {
 
 @Composable
 private fun MainScreen(uiState: UIState<MainScreenUIData>) {
-    UIStateView(state = uiState) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Cyan),
-            contentAlignment = Alignment.Center
-        ) {
-            Title(
-                title = "Home Screen",
-                style = MaterialTheme.typography.h1
-            )
+    UIScreen {
+        UIStateView(state = uiState) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                LazyColumn(
+                    contentPadding = PaddingValues(AppSpacings.M),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    item {
+                        Title(
+                            title = "Home Screen",
+                            style = MaterialTheme.typography.h1,
+                            modifier = Modifier.statusBarsPadding()
+                        )
+
+                        Spacer(modifier = Modifier.height(AppSpacings.L))
+                    }
+
+                    items(count = 100) {
+                        Title(
+                            title = "Item $it",
+                            style = MaterialTheme.typography.subtitle1,
+                        )
+                    }
+                }
+            }
         }
     }
 }
